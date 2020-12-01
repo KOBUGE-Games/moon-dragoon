22113d1b36b6a94a267beffef084b59dabce6c56
Rémi Verschelde <rverschelde@gmail.com>
2020-12-01 12:43:01 +0100

    Handle key release to skip intro so Space doesn't skip info

--- intro/intro.gd
+++ intro/intro.gd
@@ -9,7 +9,7 @@ func _ready():
 
 
 func _input(event):
-	if event is InputEventKey and not event.is_echo():
+	if event is InputEventKey and not event.is_echo() and not event.is_pressed(): # Key release.
 		set_process_input(false)  # Avoid duplicate calls.
 		get_tree().change_scene_to(level)
 		# Hack because we don't prevent stuff from starting in the global.
